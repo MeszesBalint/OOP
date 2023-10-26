2d82fd7c5f307979e7fb9b2830f0db4fb7641656
MeszesBalint <meszesbalint@gmail.com>
2023-10-26 18:20:29 +0200

    Órai

---
+++ gyumolcs.py
@@ -0,0 +1,6 @@
+fruits = ["körte", "alma", "szilva", "banán", "mangó", "ananász"]
+fruits.sort()
+
+for fruit in fruits:
+    if fruit.startswith('a'):
+        print(fruit)
---
+++ kor.py
@@ -0,0 +1,16 @@
+import math
+
+
+def kor_terulet(r):
+    return r*r*math.pi
+
+
+print(kor_terulet(8))
+
+
+def kor_kerulet(r):
+
+    return 2*r*math.pi
+
+
+print(kor_kerulet(8))
